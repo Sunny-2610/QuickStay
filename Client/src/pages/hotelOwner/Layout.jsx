1e3fed65ee12eb3f1e15416de44bf728a6a6343e
Sunny-2610 <sunnysinha9884@gmail.com>
2025-10-04 22:28:26 +0530

    completed dashboard

--- Client/src/pages/hotelOwner/Layout.jsx
+++ Client/src/pages/hotelOwner/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div>
         <Navbar />
-        <div flex h-full>
+        <div className= 'flex h-full'>
                     <Sidebar />
                     <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
                         <Outlet />
