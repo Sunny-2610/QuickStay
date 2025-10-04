0ac873d1b6d02c77fa6723ed761c78393fc7e332
Sunny-2610 <sunnysinha9884@gmail.com>
2025-10-04 19:15:44 +0530

    Setup owner Dashboard

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,10 @@ import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes('owner');
@@ -14,19 +18,26 @@ const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
       {!isOwnerPath && <Navbar />}
-         { false && <HotelReg /> }
+      {false && <HotelReg />}
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           {/* ‼️  specific route FIRST  */}
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/rooms" element={<AllRooms />} />
-                    <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path='/owner' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
+
+
+          </Route>
 
         </Routes>
       </main>
 
-      <Footer />
+      {!isOwnerPath && <Footer />}
     </div>
   );
 };
---
+++ Client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from '../../components/Title'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Title align='left' font='outfit' title='Dashboard' subtitle='Monitor your rooms listings, track booking
+        and analyze revenue-all in one place. Stay updated with real time insights to ensure smooth operations.'/>
+
+    </div>
+  )
+}
+
+export default Dashboard
