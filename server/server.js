44a70f57acfddb55d6dbc2c76e59314c214bc9c1
Sunny-2610 <sunnysinha9884@gmail.com>
2025-10-05 19:21:35 +0530

    Remove node_modules from tracking and add to .gitignore

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+
+const app = express()
+app.use(cors()) //enable Cross-Origin Resource Sharing
+app.get('/', (req,res)=>res.send("API is working fine"))
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`) )
