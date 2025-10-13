4717deb5998843676ba019c4ca0a792f92f58ee7
Sunny-2610 <sunnysinha9884@gmail.com>
2025-10-13 13:44:50 +0530

    ApI Done

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { createBooking, getHotelbookings, getUserbookings, checkAvailabilityAPI } from "../controllers/bookingControllers";
+import { createBooking, getHotelbookings, getUserbookings, checkAvailabilityAPI } from "../controllers/bookingControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 const bookingRouter = express.Router();
 
 
-bookingRouter.post('/check-availability, checkAvailabilityAPI')
+bookingRouter.post('/check-availability', checkAvailabilityAPI)
 bookingRouter.post('/book',protect, createBooking)
 bookingRouter.get('/user', protect, getUserbookings)
 bookingRouter.get('/hotel',protect, getHotelbookings)
